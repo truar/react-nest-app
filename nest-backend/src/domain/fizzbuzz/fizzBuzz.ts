56909a1af152f5ec5ab5b7e4fd5671c36eb1a613
truar <thibault.ruaro@gmail.com>
2022-04-23 10:58:28 +0200

    Add fizzbuzz and fizzbuzz generator test cases

--- nest-backend/src/domain/fizzbuzz/fizzBuzz.ts
+++ nest-backend/src/domain/fizzbuzz/fizzBuzz.ts
@@ -1,3 +1,32 @@
+interface FizzBuzzNumber {
+  MULTIPLE_OF_3_AND_5: () => string;
+  MULTIPLE_OF_3: () => string;
+  MULTIPLE_OF_5: () => string;
+  NOT_A_MULTIPLE: (n: number) => string;
+}
+
+function _fizzBuzz(input: FizzBuzzNumber): (n: number) => string {
+  return (n: number): string => {
+    if (n % 15 === 0) return input.MULTIPLE_OF_3_AND_5();
+    if (n % 3 === 0) return input.MULTIPLE_OF_3();
+    if (n % 5 === 0) return input.MULTIPLE_OF_5();
+    else return input.NOT_A_MULTIPLE(n);
+  };
+}
+
 export function fizzBuzz(input: number): string {
-  return String(input);
+  return _fizzBuzz({
+    MULTIPLE_OF_3_AND_5: () => 'fizzbuzz',
+    MULTIPLE_OF_3: () => 'fizz',
+    MULTIPLE_OF_5: () => 'buzz',
+    NOT_A_MULTIPLE: (n) => String(n),
+  })(input);
+}
+
+function allNumbersFrom1To(inclusiveEnd: number): number[] {
+  return Array.from({ length: inclusiveEnd }, (_, i) => i + 1);
+}
+
+export function fizzBuzzGenerator(inclusiveEnd: number): string[] {
+  return allNumbersFrom1To(inclusiveEnd).map((n) => fizzBuzz(n));
 }
--- nest-backend/src/domain/fizzbuzz/fizzbuzz.spec.ts
+++ nest-backend/src/domain/fizzbuzz/fizzbuzz.spec.ts
@@ -1,7 +1,56 @@
-import { fizzBuzz } from './fizzBuzz';
+import { fizzBuzz, fizzBuzzGenerator } from './fizzBuzz';
 
 describe('FizzBuzz Tests', () => {
   it('should return 1 when 1 given', () => {
     expect(fizzBuzz(1)).toBe('1');
+  });
+  it('should return 2 when 2 given', () => {
+    expect(fizzBuzz(2)).toBe('2');
+  });
+
+  describe('Multiple of 3 only', () => {
+    it('should return fizz when 3 given', () => {
+      expect(fizzBuzz(3)).toBe('fizz');
+    });
+    it('should return fizz when 6 given', () => {
+      expect(fizzBuzz(6)).toBe('fizz');
+    });
+  });
+  describe('Multiple of 5 only', () => {
+    it('should return fizz when 5 given', () => {
+      expect(fizzBuzz(5)).toBe('buzz');
+    });
+    it('should return fizz when 10 given', () => {
+      expect(fizzBuzz(10)).toBe('buzz');
+    });
+  });
+  describe('Multiple of 3 and 5', () => {
+    it('should return fizz when 15 given', () => {
+      expect(fizzBuzz(15)).toBe('fizzbuzz');
+    });
+    it('should return fizz when 30 given', () => {
+      expect(fizzBuzz(30)).toBe('fizzbuzz');
+    });
+  });
+  describe('FizzBuzzNumber Generator', () => {
+    it('should return a list of fizzBuzzNumber from 1 to end', () => {
+      expect(fizzBuzzGenerator(15)).toStrictEqual([
+        '1',
+        '2',
+        'fizz',
+        '4',
+        'buzz',
+        'fizz',
+        '7',
+        '8',
+        'fizz',
+        'buzz',
+        '11',
+        'fizz',
+        '13',
+        '14',
+        'fizzbuzz',
+      ]);
+    });
   });
 });
